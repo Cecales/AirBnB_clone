6ef1ec286774b39c87ca9daacbd32f117b396ec8
Cecales <cesar.caleroes@gmail.com>
2022-03-01 18:02:24 -0500

    Base model update

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for Base_Model class
+This class will be used for the AirBnB clone console
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+
+    """Class for base model"""
+
+    def __init__(self, *args, **kwargs):
+        
+        """Instance for a new object"""
+
+    def __str__(self):
+
+        """Returns a string representation of the object"""
+
+    def save(self):
+        
+        """Updates the public instance attribute"""
+
+    def to_dict(self):
+
+        """Returns 
+
+
