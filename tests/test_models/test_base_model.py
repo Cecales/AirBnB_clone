afc40b978f0ba7357b742ab668e6c46c6e63a716
CarlosRinconsofdev <3901@holbertonschool.com>
2022-03-06 20:12:26 -0500

    fix, test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Test Basemodel class"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+import pep8
+
+class test_base(unittest.TestCase):
+    """
+    class testing BaseModel class' methods
+    """
+    @classmethod
+    def setUpClass(cls):
+        """Set up class method"""
+        cls.instance = BaseModel()
+
+    def tearDownClass(cls):
+        """Final step"""
+        del cls.instance
+    
+    def test_pep8_conformance(self):
+        """Test that base_model.py file conform to PEP8"""
+        pep8style = pep8.StyleGuide(quite=True)
+        result = pep8style.checkfiles(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_doc(self):
+        """Test documentation BaseModel"""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    
+if __name__ == '__main__':
+    unittest.main()
