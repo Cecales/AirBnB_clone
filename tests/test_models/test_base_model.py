d9df4cc1115139e30191835914899ba5587d483d
CarlosRinconsofdev <3901@holbertonschool.com>
2022-03-07 22:09:38 -0500

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Test Basemodel class"""
-import pep8
+"""Test BaseModel class"""
 import unittest
+import pep8
 from datetime import datetime
 from models.base_model import BaseModel
+
 
 class test_base(unittest.TestCase):
     """
@@ -14,10 +15,11 @@ class test_base(unittest.TestCase):
         """Set up class method"""
         cls.instance = BaseModel()
 
+    @classmethod
     def tearDownClass(cls):
         """Final step"""
         del cls.instance
-    
+
     def test_pep8_conformance(self):
         """Test that base_model.py file conform to PEP8"""
         pep8style = pep8.StyleGuide(quite=True)
@@ -31,6 +33,34 @@ class test_base(unittest.TestCase):
         self.assertIsNotNone(BaseModel.__str__.__doc__)
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
-    
+
+    def test_base_atrr(self):
+        """Test BaseModel attributes"""
+        base = BaseModel()
+        self.assertIsInstance(base.id, str)
+        self.assertEqual(len(base.id), 36)
+        self.assertIsInstance(base.created_at, datetime.datetime)
+        self.assertIsInstance(base.updated_at, datetime.datetime)
+
+    def test_save(self):
+        """Test save method"""
+        past_update = self.instance.updated_at
+        self.instance.save()
+        self.assertIsNot(self.instance.created_at, self.instance.updated_at)
+        self.assertIsNot(past_update, self.instance.updated_at)
+
+    def test_base_model_to_dict_method(self):
+        """BaseModel to_dict method creates accurate dictionary"""
+        base = BaseModel()
+        base_dict = base.to_dict()
+        self.assertIsInstance(base_dict, dict)
+        self.assertEqual(base_dict['id'], base.id)
+        self.assertEqual(base_dict['__class__'], type(base).__name__)
+        self.assertEqual(base_dict['created_at'], base.created_at.isoformat())
+        self.assertEqual(base_dict['updated_at'], base.updated_at.isoformat())
+        self.assertIsInstance(base.created_at, datetime.datetime)
+        self.assertIsInstance(base.updated_at, datetime.datetime)
+
+
 if __name__ == '__main__':
     unittest.main()
